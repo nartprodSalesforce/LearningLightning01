badd748ecaece43f83906a088327d16af344c874
Stuart <swerner@salesforce.com>
2018-11-01 13:40:38 -0400

    Completed Learning 1

--- force-app/main/default/aura/NameForm/NameFormController.js
+++ force-app/main/default/aura/NameForm/NameFormController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
-
+    clickCreate : function(component, event, helper) {
+        var newNamelocal = component.get("v.newName");
+        //alert("Hello " + newNamelocal);
+        var thenames = component.get("v.names"); //clone of the  collection in view 
+        thenames.push(newNamelocal);
+        component.set("v.names", thenames);
     }
 })
